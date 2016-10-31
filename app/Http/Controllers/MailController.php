6c14d9cd5807add8ada0e69b41578337fbad35f6
Sarath <sarathiscookie@hotmail.com>
2016-10-31 21:19:01 +0530

    Email: Added later function

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -12,12 +12,16 @@ use App\Mail\WelcomeMessage;
 
 use Mail;
 
+use Carbon\Carbon;
+
 class MailController extends Controller
 {
     public function index()
     {
+        $when = Carbon::now()->addMinutes(1);
         $user = User::find(1);
-        Mail::to($user->email)->queue(new WelcomeMessage($user));
+        /*Mail::to($user->email)->queue(new WelcomeMessage($user));*/
+        Mail::to($user->email)->later($when, new WelcomeMessage($user));
         return redirect()->back();
     }
 
